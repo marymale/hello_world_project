8ba126df6a30b93a687180874fcf4a7d8d947bd2
marymale <136014016@qq.com>
2017-11-15 22:41:10 +0800

    hello world

--- controller/core_controller.py
+++ controller/core_controller.py
@@ -1,8 +1,8 @@
 # coding=utf-8
 import re
 import json
-import urllib
-import urllib2
+import requests
+from Crypto.Cipher import AES
 from the_project import PROJECT_DIR
 from model.key_manager import KeyManager
 from model.bot_manager import BotManager
@@ -12,17 +12,31 @@ class CoreController(object):
     def __init__(self):
         with open('{}/controller/conf.json'.format(PROJECT_DIR)) as f:
             self.conf = json.load(f)
-            self.key_manager = KeyManager()
-            self.bot_manager = BotManager()
+        self.encryption = AES.new('marymale key2333', AES.MODE_CBC, 'This is marymale')
+        self.key_manager = KeyManager()
+        self.bot_manager = BotManager()
+        self.req_text = ''
+        self.res_text = ''
 
     def connector(self, command):
-        text = urllib.urlencode({'command': command})
-        req = urllib2.Request(url='{}?{}'.format(self.conf['url'], text))
-        return urllib2.urlopen(req).read().strip()
+        url = self.conf['url']
+        d = {'command': command}
+        r = requests.post(url, data=d)
+        self.res_text = r.text.strip()
 
-    @staticmethod
-    def transmitter(the_text):
-        pass
+    def transmitter(self, _blueprint):
+        def _2fa():
+            op, bot_name = _blueprint.split(' ')[1:3]
+            op_dict = {'1': '2fa', '2': '2faok', '3': '2fano'}
+            return '{} {}'.format(str(op_dict[op]), str(bot_name))
+
+        def _owns():
+            bot_name, game_id = _blueprint.split(' ')[1:3]
+            return 'owns {} {}'.format(bot_name, game_id)
+
+        run_dict = {'2fa': _2fa, 'owns': _owns}
+
+        self.req_text = self.encryption.encrypt(run_dict[_blueprint.split(' ')[0]]())
 
     @staticmethod
     def receiver(the_text):
@@ -39,13 +53,22 @@ class CoreController(object):
         res_list = []
         _2fa_receiver()
         _owns_receiver()
-        pass
+
+        print res_list
+
+    def generator(self, blueprint):
+        self.transmitter(blueprint)
+        self.connector(self.req_text)
+        self.receiver(self.res_text)
 
 
 if __name__ == '__main__':
     cc = CoreController()
-    res = cc.connector('owns marymale0 *')
-    print res
-    cc.receiver(res)
+    cc.generator('owns marymale0 *')
+    print cc.req_text
+    print cc.res_text
+    # cc.connector('2fa marymale0')
+    # print cc.res_text
+    # cc.receiver(res)
     # cc.bot_manager.get_all_bot_name()
     pass
