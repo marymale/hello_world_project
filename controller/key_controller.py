04fbf9d4cc4164a81e2144d36dcfe234cae3de11
marymale <136014016@qq.com>
2017-11-20 15:26:28 +0800

    hello world

--- controller/key_controller.py
+++ controller/key_controller.py
@@ -1,14 +1,10 @@
 # coding=utf-8
 import re
 import json
-import requests
-import urllib
-from the_project import PROJECT_DIR
 from model.key_manager import KeyManager
-from model.bot_manager import BotManager
 
 
-class CoreController(object):
+class KeyController(object):
     def __init__(self):
         self.key_manager = KeyManager()
 
@@ -23,4 +19,19 @@ class CoreController(object):
         print 'key    : ', res
         return _key
 
-    def add_key_list(self, text):
+    def add_key_list(self, the_text, level, game_id):
+        key_list = self.detection_key(the_text)
+        for key in key_list:
+            self.key_manager.add_key(key, level, game_id)
+
+if __name__ == '__main__':
+    kc = KeyController()
+    text = 'TPDTZ-D7N8B-4CN3Z, J4MM3-TWTHW-0IM94, TMNMA-I64KZ-D685W, 3R67F-TFPED-0AVAV, 7I7BW-H436D-XNP5G,'
+    kc.key_manager.del_database()
+    kc.add_key_list(text, 2, 'null')
+    kc.key_manager.del_key('J4MM3-TWTHW-0IM94')
+    kc.key_manager.update_key('TPDTZ-D7N8B-4CN3Z', 1, '09090')
+    print kc.key_manager.get_key('TPDTZ-D7N8B-4CN3Z')
+    print kc.key_manager.get_level(2)
+    print kc.key_manager.get_game_id('09090')
+    pass
--- model/key_manager.py
+++ model/key_manager.py
@@ -1,7 +1,5 @@
 # coding=utf
-import re
 import json
-import pyperclip
 from the_project import PROJECT_DIR
 
 
@@ -24,23 +22,15 @@ class KeyManager(object):
 
     def del_database(self):
         self.database = dict()
+        self.database.setdefault('key_logic', dict())
+        self.database.setdefault('level_logic', dict())
+        self.database.setdefault('game_logic', dict())
         with open(self.database_name, 'w+') as f:
             json.dump(self.database, f, indent=2)
 
     def storage_database(self):
         with open(self.database_name, 'w+') as f:
             json.dump(self.database, f, indent=2)
-
-    @staticmethod
-    def detection_key(keys):
-        arr, temp = [], keys.strip().split()
-        for each in temp:
-            arr.extend(each.split(','))
-        _key = [each for each in arr if re.match(r'.{5}-.{5}-.{5}', each) is not None]
-        res = ','.join(_key)
-        print 'key num: ', len(_key)
-        print 'key    : ', res
-        return _key
 
     def add_key(self, key, level, game_id):
         level, game_id = str(level), str(game_id)
@@ -69,29 +59,26 @@ class KeyManager(object):
         level, game_id = str(level), str(game_id)
         op = False
         if self.del_key(key):
-            self.database['key_logic'].setdefault(key, {'level': level, 'game_id': game_id})
-            self.database['level_logic'].setdefault(level, dict())
-            self.database['level_logic'][level].setdefault(key, game_id)
-            self.database['game_logic'].setdefault(game_id, dict())
-            self.database['game_logic'][game_id].setdefault(key, level)
-            self.storage_database()
-            op = True
+            if self.add_key(key, level, game_id):
+                op = True
         return op
 
     def get_key(self, key):
         if self.database['key_logic'].get(key) is not None:
-            return {'key': key, 'level': self.database['key_logic'][key]['level'],
-                    'game_id': self.database['key_logic'][key]['game_id']}
+            return [{'key': key, 'level': self.database['key_logic'][key]['level'],
+                    'game_id': self.database['key_logic'][key]['game_id']}]
         return None
 
+    def get_level(self, level):
+        level = str(level)
+        if self.database['level_logic'].get(level) is not None:
+            return [{'key': i, 'level': level, 'game_id': self.database['level_logic'][level][i]} for i in
+                    self.database['level_logic'][level].keys()]
+        return None
 
-if __name__ == '__main__':
-    km = KeyManager()
-    text = pyperclip.paste()
-    text = 'TPDTZ-D7N8B-4CN3Z, J4MM3-TWTHW-0IM94, TMNMA-I64KZ-D685W, 3R67F-TFPED-0AVAV, 7I7BW-H436D-XNP5G,'
-    km.del_database()
-    km.add_key(text, 2, 'null')
-    km.del_key('J4MM3-TWTHW-0IM94')
-    km.update_key('TPDTZ-D7N8B-4CN3Z', 1, '09090')
-    print km.get_key('TPDTZ-D7N8B-4CN3Z')
-    pass
+    def get_game_id(self, game_id):
+        game_id = str(game_id)
+        if self.database['game_logic'].get(game_id) is not None:
+            return [{'key': i, 'level': self.database['game_logic'][game_id][i], 'game_id': game_id} for i in
+                    self.database['game_logic'][game_id].keys()]
+        return None
