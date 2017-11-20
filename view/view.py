0c8063b922797f0775d42fe53fb38941f5d78e59
marymale <136014016@qq.com>
2017-11-20 17:07:07 +0800

    hello world

---
+++ view/view.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+import pyperclip
+from controller.core_controller import CoreController
+
+
+class View(object):
+    def __init__(self):
+        self.controller = CoreController()
+
+    def update_all_owns(self):
+        self.controller.generator('owns')
+
+    def update_bot_owns(self, bot_name):
+        self.controller.generator('owns {} *'.format(bot_name))
+
+    def add_keys(self, key_text, level, game_id):
+        key_text = key_text.replace('\n', ',')
+        key_text = key_text.replace(' ', ',')
+        self.controller.generator('addkey {} {} {}'.format(key_text, level, game_id))
+
+
+if __name__ == '__main__':
+    v = View()
+    # v.add_keys(pyperclip.paste(), 0, 'null')
